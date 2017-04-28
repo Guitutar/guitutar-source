2917b4e35ebb1abd5ff79cbda6777b9de88d638e
Austin Peterson <peter174@purdue.edu>
2017-04-28 02:06:05 -0400

    Added demo program, note validation, switch matrix mappings, and rolling averaging

--- guitutar.c
+++ guitutar.c
@@ -53,6 +53,45 @@ PORTS_CHANNEL getChannelFretOutputClk(int fretNum) {
     return PORT_CHANNEL_D;
 }
 
+PORTS_CHANNEL getChannelFretSwitch(int fretNum) {
+    switch(fretNum){
+        // PCB 1 Decoding
+        case 1:
+            return PORT_CHANNEL_E;
+        case 2:
+            return PORT_CHANNEL_A;
+        case 3:
+            return PORT_CHANNEL_A;
+        // PCB 2 Decoding
+        case 4:
+            return PORT_CHANNEL_G;
+        case 5:
+            return PORT_CHANNEL_A;
+        case 6:
+            return PORT_CHANNEL_E;
+        // PCB 3 Decoding
+        case 7:
+            return PORT_CHANNEL_E;
+        case 8:
+            return PORT_CHANNEL_B;
+        case 9:
+            return PORT_CHANNEL_B;
+        case 10:
+            return PORT_CHANNEL_B;
+        // PCB 4 Decoding
+        case 11:
+            return PORT_CHANNEL_B;
+        case 12:
+            return PORT_CHANNEL_B;
+        case 13:
+            return PORT_CHANNEL_B;
+        case 14:
+            return PORT_CHANNEL_A;
+        case 15:
+            return PORT_CHANNEL_A;
+    }
+}
+
 PORTS_BIT_POS getBitPosFretSerial(int fretNum) {
 //    return PORTS_BIT_POS_4;
     switch(fretNum){
@@ -103,6 +142,45 @@ PORTS_BIT_POS getBitPosFretOutputClk(int fretNum) {
     return PORTS_BIT_POS_8;
 }
 
+PORTS_BIT_POS getBitPosFretSwitch(int fretNum) {
+    switch(fretNum){
+        // PCB 1 Decoding
+        case 1:
+            return PORTS_BIT_POS_0;
+        case 2:
+            return PORTS_BIT_POS_7;
+        case 3:
+            return PORTS_BIT_POS_6;
+        // PCB 2 Decoding
+        case 4:
+            return PORTS_BIT_POS_0;
+        case 5:
+            return PORTS_BIT_POS_0;
+        case 6:
+            return PORTS_BIT_POS_8;
+        // PCB 3 Decoding
+        case 7:
+            return PORTS_BIT_POS_9;
+        case 8:
+            return PORTS_BIT_POS_5;
+        case 9:
+            return PORTS_BIT_POS_4;
+        case 10:
+            return PORTS_BIT_POS_3;
+        // PCB 4 Decoding
+        case 11:
+            return PORTS_BIT_POS_2;
+        case 12:
+            return PORTS_BIT_POS_6;
+        case 13:
+            return PORTS_BIT_POS_7;
+        case 14:
+            return PORTS_BIT_POS_9;
+        case 15:
+            return PORTS_BIT_POS_10;
+    }
+}
+
 Note createNote(int fretNum, String string) {
     Note note;
     note.fret = fretNum;
@@ -141,6 +219,45 @@ void shiftRegClear() {
     SYS_PORTS_PinWrite(PORTS_ID_0, PORT_CHANNEL_A, PORTS_BIT_POS_14, true);
 }
 
+void runDemo() {
+    int i, j, k;
+    while(true) {
+        for(i = 7; i < 11; ++i) { // frets 0-15
+//        i = 8;    
+            for(j = 128; j >= 4; j /= 2) {
+//            j = 128;
+                shiftRegClear();
+                clockPin(PORT_CHANNEL_D, PORTS_BIT_POS_8);
+                //for (k = 0; k < CONST_WAIT_TIME; k++){}
+                displayNote(j, i);
+                for (k = 0; k < CONST_WAIT_TIME; k++){}
+//                shiftRegClear();
+//                displayNote(j*2, i);
+//                for (k = 0; k < CONST_WAIT_TIME; k++){}
+//                shiftRegClear();
+//                displayNote(j*4, i);
+//                for (k = 0; k < CONST_WAIT_TIME; k++){}
+//                shiftRegClear();
+//                displayNote(j*8, i);
+//                for (k = 0; k < CONST_WAIT_TIME; k++){}
+//                shiftRegClear();
+//                displayNote(j*16, i);
+//                for (k = 0; k < CONST_WAIT_TIME; k++){}
+//                shiftRegClear();
+//                displayNote(j*32, i);
+//                for (k = 0; k < CONST_WAIT_TIME; k++){}
+            }
+        }
+    }
+}
+
+bool isNoteBeingPlayed(int fretNum) {
+    PORTS_CHANNEL noteChannel = getChannelFretSwitch(fretNum);
+    PORTS_BIT_POS noteBitPos = getBitPosFretSwitch(fretNum);
+    
+    return !(SYS_PORTS_PinRead(PORTS_ID_0, noteChannel, noteBitPos));
+}
+
 void emptyFretboard(uint8_t* fretboardArray, int numFrets) {
     int i;
     for(i = 0; i < numFrets; ++i)
@@ -154,3 +271,36 @@ void updateFretboard(uint8_t* fretboardArray, Note note) {
 void displayFretboard(uint8_t* fretboardArray) {
     
 }
+
+RollAvg newRollAvg(){
+    RollAvg result;
+    result.edge = 0;
+    initRollAvg(&result);
+    return result;
+}
+void initRollAvg(RollAvg *raPtr){
+    int i;
+    for (i = 0; i < AVG_SAMPLE_SIZE; i++){
+        raPtr->samples[i] = false;
+    }
+    raPtr->total = 0;
+}
+void addToAvg(RollAvg *raPtr, bool value){
+    if(raPtr->samples[raPtr->edge] && !value){
+        raPtr->total--;
+        raPtr->samples[raPtr->edge] = value;
+    }
+    else if (!raPtr->samples[raPtr->edge] && value){
+        raPtr->total++;
+        raPtr->samples[raPtr->edge] = value;
+    }
+    raPtr->edge = ++raPtr->edge % AVG_SAMPLE_SIZE;
+}
+bool isAvgOn(RollAvg *raPtr){
+    return (raPtr->total >= AVG_THRESHOLD);
+}
+
+//COLE
+//Chord *newChord(){
+    
+//}
